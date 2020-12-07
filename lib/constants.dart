2d939153a0d7b658e66e13bd73a2fdf25f03d59c
hu <C:\Users\hu\AppData\Roaming\The Bat!>
2020-12-07 22:16:13 +0400

    email validator

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,3 +6,12 @@ const kTextColor = Color(0xFF575E67);
 const kBackgroundColor = Color(0xFFFCFAF8);
 
 const kAnimationDuration = Duration(milliseconds: 200);
+
+//Form error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = 'Пожалуйста, введите Вашу почту';
+const String kInvalidEmailError = 'Пожалуйста, введите валидную почту';
+const String kPassNullError = 'Пожалуйста, введите Ваш пароль';
+const String kShortPassError = 'Пароль слишком короткий';
+const String kMatchPassError = 'Пароли не совпадают';
--- lib/screens/sign_in/components/body_sign_in.dart
+++ lib/screens/sign_in/components/body_sign_in.dart
@@ -1,7 +1,9 @@
+import 'package:cookie/components/custom_surfix_icon.dart';
+import 'package:cookie/components/default_button.dart';
+import 'package:cookie/components/form_error.dart';
 import 'package:cookie/constants.dart';
 import 'package:cookie/size_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class BodySignIn extends StatelessWidget {
   @override
@@ -41,30 +43,87 @@ class SignForm extends StatefulWidget {
 }
 
 class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
-          TextFormField(
-              decoration: InputDecoration(
-            labelText: 'Почта',
-            hintText: 'Введите Ваш почтовый адрес',
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            suffixIcon: Padding(
-              padding: EdgeInsets.fromLTRB(
-                0,
-                getProportionateScreenWidth(20),
-                getProportionateScreenWidth(20),
-                getProportionateScreenWidth(20),
-              ),
-              child: SvgPicture.asset(
-                'assets/icons/Mail.svg',
-                height: getProportionateScreenWidth(18),
-              ),
-            ),
-          ))
+          buildEmailFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          buildPasswordFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          FormError(errors: errors),
+          DefaultButton(
+            text: 'Продолжить',
+            press: () {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+              }
+            },
+          ),
         ],
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Пароль',
+        hintText: 'Введите пароль',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: 'assets/icons/Lock.svg',
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.remove(kEmailNullError);
+          });
+        } else if (emailValidatorRegExp.hasMatch(value) &&
+            errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.remove(kInvalidEmailError);
+          });
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.add(kEmailNullError);
+          });
+        } else if (!emailValidatorRegExp.hasMatch(value) &&
+            !errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.add(kInvalidEmailError);
+          });
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: 'Почта',
+        hintText: 'Введите Ваш почтовый адрес',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: 'assets/icons/Mail.svg',
+        ),
       ),
     );
   }
