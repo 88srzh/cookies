2cfd27e85606100b9feee9b464e6dc3adc42af90
hu <smpetrov18@yandex.ru>
2020-12-10 20:05:35 +0400

    sign up screen

--- lib/screens/sign_up/components/body_sign_up.dart
+++ lib/screens/sign_up/components/body_sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:cookie/components/custom_surfix_icon.dart';
+import 'package:cookie/components/default_button.dart';
 import 'package:cookie/constants.dart';
 import 'package:cookie/size_config.dart';
 import 'package:flutter/material.dart';
@@ -8,18 +9,22 @@ class BodySignUp extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [
-          Text(
-            'Зарегистрироваться',
-            style: headingStyle,
-          ),
-          Text(
-            'Заполните анкету или продолжите \nчерез социальные сети',
-            textAlign: TextAlign.center,
-          ),
-          SignUpForm(),
-        ],
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: Column(
+          children: [
+            Text(
+              'Зарегистрироваться',
+              style: headingStyle,
+            ),
+            Text(
+              'Заполните анкету или продолжите \nчерез социальные сети',
+              textAlign: TextAlign.center,
+            ),
+            SignUpForm(),
+          ],
+        ),
       ),
     );
   }
@@ -56,39 +61,123 @@ class _SignUpFormState extends State<SignUpForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onSaved: (newValue) => email = newValue,
-            onChanged: (value) {
-              if (value.isNotEmpty) {
-                removeError(error: kEmailNullError);
-              } else if (emailValidatorRegExp.hasMatch(value)) {
-                removeError(error: kInvalidEmailError);
+          buildEmailFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildConfirmPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(40)),
+          DefaultButton(
+            text: 'Продолжить',
+            press: () {
+              if (_formKey.currentState.validate()) {
+                // Go to complete profile page
               }
-              return null;
             },
-            validator: (value) {
-              if (value.isEmpty) {
-                addError(error: kEmailNullError);
-                return "";
-              } else if (!emailValidatorRegExp.hasMatch(value)) {
-                addError(error: kInvalidEmailError);
-                return "";
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              labelText: 'Почта',
-              hintText: 'Введите Вашу почту',
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: CustomSurffixIcon(
-                svgIcon: 'assets/icons/Mail.svg',
-              ),
-            ),
           ),
         ],
+      ),
+    );
+  }
+
+  TextFormField buildConfirmPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => confirm_password = newValue,
+      onChanged: (value) {
+        if (password == confirm_password) {
+          removeError(error: kMatchPassError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          return "";
+        } else if (password != confirm_password) {
+          addError(error: kMatchPassError);
+          return "";
+        }
+        return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Подтвердите пароль',
+        hintText: 'Повторите пароль',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: 'assets/icons/Lock.svg',
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kPassNullError);
+        } else if (value.length >= 8) {
+          removeError(error: kShortPassError);
+        }
+        password = value;
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: kPassNullError);
+          return "";
+        } else if (value.length < 8) {
+          addError(error: kShortPassError);
+          return "";
+        }
+        return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Пароль',
+        hintText: 'Введите пароль',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: 'assets/icons/Lock.svg',
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kEmailNullError);
+        } else if (emailValidatorRegExp.hasMatch(value)) {
+          removeError(error: kInvalidEmailError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: kEmailNullError);
+          return "";
+        } else if (!emailValidatorRegExp.hasMatch(value)) {
+          addError(error: kInvalidEmailError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: 'Почта',
+        hintText: 'Введите Вашу почту',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: 'assets/icons/Mail.svg',
+        ),
       ),
     );
   }
