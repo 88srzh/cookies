ff3f3212f5d95c3217c6c2750f49e3d1b81591fc
srzh <smpetrov18@yandex.ru>
2020-12-14 23:50:58 +0400

    start sweets

---
+++ lib/models/sweets.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Sweets {
+  final String title, description;
+  final List<String> images;
+  final List<Color> colors;
+  final double rating, price;
+  final bool isFavourite, isPopular;
+
+  Sweets({
+    @required this.images,
+    @required this.colors,
+    this.rating = 0.0,
+    this.isFavourite = false,
+    this.isPopular = false,
+    @required this.title,
+    @required this.price,
+    @required this.description,
+  });
+}
--- lib/screens/home/components/body_home.dart
+++ lib/screens/home/components/body_home.dart
@@ -1,6 +1,7 @@
 import 'package:cookie/screens/home/components/categories.dart';
 import 'package:cookie/screens/home/components/discount_banner.dart';
 import 'package:cookie/screens/home/components/home_header.dart';
+import 'package:cookie/screens/home/components/section_title.dart';
 import 'package:cookie/screens/home/components/special_offers.dart';
 import 'package:cookie/size_config.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,9 @@ class BodyHome extends StatelessWidget {
             Categories(),
             SizedBox(height: getProportionateScreenWidth(30)),
             SpecialOffers(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            SectionTitle(text: 'Популярное', press: () {}),
+            SizedBox(height: getProportionateScreenWidth(30)),
           ],
         ),
       ),
