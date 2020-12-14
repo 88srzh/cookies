97428af9d67a6d0fef03f45ead003bfb9d9d432b
hu <smpetrov18@yandex.ru>
2020-12-14 16:52:52 +0400

    body home

--- lib/screens/home/components/body_home.dart
+++ lib/screens/home/components/body_home.dart
@@ -3,22 +3,56 @@ import 'package:cookie/screens/home/components/home_header.dart';
 import 'package:cookie/size_config.dart';
 import 'package:flutter/material.dart';
 
-import '../../../bottom_bar.dart';
-import '../../../cookie_page.dart';
-import '../../../icecream_page.dart';
-
 class BodyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
         child: Column(
-          children: [
+          children: <Widget>[
             SizedBox(height: getProportionateScreenWidth(20)),
             HomeHeader(),
             SizedBox(height: getProportionateScreenWidth(30)),
             DiscountBanner(),
+            BodyHomeScreen(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class BodyHomeScreen extends StatefulWidget {
+  BodyHomeScreen({Key key}) : super(key: key);
+
+  @override
+  _BodyHomeScreenState createState() => _BodyHomeScreenState();
+}
+
+class _BodyHomeScreenState extends State<BodyHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: 'Dog',
+                  child: Text(
+                    '123',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                Tab(
+                  text: 'Cat',
+                ),
+                Tab(text: 'Frog'),
+              ],
+            ),
+          ),
         ),
       ),
     );
