291e78fa4628403716d847b1b5dce9d3b1f7dea9
hu <smpetrov18@yandex.ru>
2020-12-17 16:57:16 +0400

    cart

--- lib/screens/cart/components/body_cart.dart
+++ lib/screens/cart/components/body_cart.dart
@@ -1,14 +1,30 @@
 import 'package:cookie/models/Cart.dart';
 import 'package:cookie/screens/cart/components/cart_item_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class BodyCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CartItemCard(
-          cart: carts[0],
+        Dismissible(
+          key: Key(carts[0].sweets.id.toString()),
+          background: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+              color: Color(0xFFFFE6E6),
+            ),
+            child: Row(
+              children: [
+                Spacer(),
+                SvgPicture.asset('assets/icons/Trash.svg'),
+              ],
+            ),
+          ),
+          child: CartItemCard(
+            cart: carts[0],
+          ),
         ),
       ],
     );
