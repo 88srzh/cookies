39f5b750eff07e9f7e15539e759baabfe5c55d00
srzh <smpetrov18@yandex.ru>
2020-12-17 08:30:59 +0400

    cart item card

---
+++ lib/screens/cart/components/cart_item_card.dart
@@ -0,0 +1,65 @@
+import 'package:cookie/models/Cart.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class CartItemCard extends StatelessWidget {
+  const CartItemCard({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: getProportionateScreenWidth(88),
+          child: AspectRatio(
+            aspectRatio: 0.88,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F6F9),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Image.asset(cart.sweets.images[0]),
+            ),
+          ),
+        ),
+        SizedBox(width: getProportionateScreenWidth(20)),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              cart.sweets.title,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+              maxLines: 2,
+            ),
+            const SizedBox(height: 10),
+            Text.rich(
+              TextSpan(
+                  text: '${cart.sweets.price}p',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: kPrimaryColor,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: ' x${cart.numOfItems}',
+                      style: TextStyle(color: kTextColor),
+                    ),
+                  ]),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
