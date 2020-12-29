235fd9f05b272b6dc7a021edcda886b7eb2d37dc
hu <smpetrov18@yandex.ru>
2020-12-29 16:40:42 +0400

    consumer change

--- lib/screens/cart/components/cart_item_card.dart
+++ lib/screens/cart/components/cart_item_card.dart
@@ -1,5 +1,7 @@
 import 'package:cookie/models/Cart.dart';
+import 'package:cookie/models/sweets.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../constants.dart';
 import '../../../size_config.dart';
@@ -12,55 +14,61 @@ class CartItemCard extends StatelessWidget {
 
   final Cart cart;
 
+  // final cart = Cart(sweets: allSweets[0],numOfItems: 2);
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          width: getProportionateScreenWidth(88),
-          child: AspectRatio(
-            aspectRatio: 0.88,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Color(0xFFF5F6F9),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Image.asset(cart.sweets.images[0]),
-            ),
-          ),
-        ),
-        SizedBox(width: getProportionateScreenWidth(20)),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return Consumer<Cart>(
+      builder: (context, cart, child) {
+        return Row(
           children: [
-            Text(
-              cart.sweets.title,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-              ),
-              maxLines: 2,
-            ),
-            const SizedBox(height: 10),
-            Text.rich(
-              TextSpan(
-                text: '${cart.sweets.price}p',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: kPrimaryColor,
-                ),
-                children: [
-                  TextSpan(
-                    text: ' x${cart.numOfItems}',
-                    style: TextStyle(color: kTextColor),
+            SizedBox(
+              width: getProportionateScreenWidth(88),
+              child: AspectRatio(
+                aspectRatio: 0.88,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF5F6F9),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                ],
+                  child: Image.asset(cart.sweets.images[0]),
+                ),
               ),
+            ),
+            SizedBox(width: getProportionateScreenWidth(20)),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  cart.sweets.title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                  maxLines: 2,
+                ),
+                const SizedBox(height: 10),
+                Text.rich(
+                  TextSpan(
+                    text: '${cart.sweets.price}p',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: kPrimaryColor,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: ' x${cart.numOfItems}',
+                        style: TextStyle(color: kTextColor),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
-        ),
-      ],
+        );
+      },
     );
   }
 }
