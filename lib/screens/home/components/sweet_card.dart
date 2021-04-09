481c1fee5ebd21e596319e1e9408f2dda1fb0650
srzh <smpetrov18@yandex.ru>
2021-04-10 00:30:43 +0400

    fix sweet card

--- lib/screens/home/components/sweet_card.dart
+++ lib/screens/home/components/sweet_card.dart
@@ -36,9 +36,12 @@ class _SweetCardState extends State<SweetCard> {
     var sweet = Provider.of<Sweet>(context);
     return Container(
       decoration: BoxDecoration(
-        color: Color.fromRGBO(248, 242, 244, 0.5),
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          // stops: [0.3, 1.8],
+          colors: [Color.fromRGBO(248, 219, 221, 1.0), Colors.orange[100]],
         ),
       ),
       child: GestureDetector(
@@ -48,91 +51,109 @@ class _SweetCardState extends State<SweetCard> {
         },
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(241, 240, 246, 2.0),
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(20),
-                      bottomLeft: Radius.circular(30),
+            Flexible(
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(241, 240, 246, 2.0),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(30),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: getProportionateScreenWidth(20),
+                          vertical: getProportionateScreenWidth(12),
+                        ),
+                        child: Text(
+                          '${sweet.price}₽',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
                     ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  bottom: getProportionateScreenWidth(10),
+                ),
+                child: SizedBox(
+                  width: getProportionateScreenWidth(100),
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Image.asset(sweet.images),
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: getProportionateScreenWidth(20),
-                      vertical: getProportionateScreenWidth(12),
-                    ),
-                    child: Text(
-                      '${sweet.price}₽',
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${sweet.title}',
+                      textAlign: TextAlign.center,
                       style: TextStyle(
+                        fontSize: 16,
                         fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      '${sweet.categories}',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: getProportionateScreenWidth(15),
+                    right: getProportionateScreenWidth(15),
+                    top: getProportionateScreenWidth(15),
+                  ),
+                  child: Flexible(
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InkWell(
+                            splashColor: Colors.transparent,
+                            onTap: () {
+                              // tapFavourite();
+                            },
+                            child: sweet.isFavorite
+                                ? Icon(Icons.favorite)
+                                : Icon(Icons.favorite_outline),
+                          ),
+                          Text(
+                            '${sweet.rating}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                bottom: getProportionateScreenWidth(10),
-              ),
-              child: SizedBox(
-                width: getProportionateScreenWidth(100),
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: Image.asset(sweet.images),
-                ),
-              ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '${sweet.title}',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                  ),
-                ),
-                Text(
-                  '${sweet.categories}',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: getProportionateScreenWidth(15),
-                right: getProportionateScreenWidth(15),
-                top: getProportionateScreenWidth(15),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    splashColor: Colors.transparent,
-                    onTap: () {
-                      // tapFavourite();
-                    },
-                    child: sweet.isFavorite
-                        ? Icon(Icons.favorite)
-                        : Icon(Icons.favorite_outline),
-                  ),
-                  Text(
-                    '${sweet.rating}',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ],
               ),
             ),
           ],
