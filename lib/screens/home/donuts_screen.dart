ad3c32367c1afbc39299a4cb818758ab2b7b4341
srzh <smpetrov18@yandex.ru>
2021-04-11 10:35:37 +0400

    fix flexible in donuts screen

--- lib/screens/home/donuts_screen.dart
+++ lib/screens/home/donuts_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cookie/components/default_button_grey.dart';
 import 'package:cookie/models/items.dart';
 import 'package:cookie/models/items_info.dart';
+import 'package:cookie/screens/description/descriprion_screen.dart';
 import 'package:cookie/screens/home/components/item_card.dart';
 import 'package:cookie/size_config.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -77,29 +78,21 @@ class DonutsScreen extends StatelessWidget {
                     FirebaseFirestore.instance.collection('Items').snapshots(),
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) return const Text('Loading...');
-                  return GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2),
-                      itemCount: snapshot.data.docs.length,
-                      itemBuilder: (context, index) =>
-                          buildListItem(context, snapshot.data.docs[index]));
                   // -----------------------------------------------------
-                  // return GridView.count(
-                  //   crossAxisCount: 2,
-                  //   shrinkWrap: true,
-                  //   mainAxisSpacing: 15.0,
-                  //   crossAxisSpacing: 20.0,
-                  //   childAspectRatio:
-                  //       (SizeConfig.itemWidth / SizeConfig.itemHeight),
-                  //   children: [
-                  //     ...List.generate(snapshot.data.documents.length, (index) {
-                  //       return ChangeNotifierProvider.value(
-                  //         value: snapshot.data.documents[index],
-                  //         child: ItemCard(),
-                  //       );
-                  //     })
-                  //   ],
-                  // );
+                  return GridView.count(
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    mainAxisSpacing: 15.0,
+                    crossAxisSpacing: 20.0,
+                    childAspectRatio:
+                        (SizeConfig.itemWidth / SizeConfig.itemHeight),
+                    children: [
+                      ...List.generate(snapshot.data.docs.length, (index) {
+                        return buildItemCard(
+                            context, snapshot.data.docs[index]);
+                      })
+                    ],
+                  );
                 },
               ),
             ),
@@ -136,6 +129,143 @@ class DonutsScreen extends StatelessWidget {
           ),
         ),
       ]),
+    );
+  }
+
+  Widget buildItemCard(BuildContext context, DocumentSnapshot docs) {
+    return Container(
+      // height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          // stops: [0.3, 1.8],
+          colors: [Color.fromRGBO(248, 219, 221, 1.0), Colors.orange[100]],
+        ),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          // Navigator.of(context)
+          //     .pushNamed(DescriptionScreen.routeName, arguments: docs.id);
+        },
+        child: Column(
+          children: [
+            Flexible(
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(241, 240, 246, 2.0),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(30),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: getProportionateScreenWidth(20),
+                          vertical: getProportionateScreenWidth(12),
+                        ),
+                        child: Text(
+                          '${docs['price'].toString()}₽',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: getProportionateScreenWidth(10),
+                  ),
+                  child: SizedBox(
+                    width: getProportionateScreenWidth(100),
+                    child: AspectRatio(
+                      aspectRatio: 1,
+                      // child: Image.asset(item.images),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        '${docs['title']}',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        '${docs['categories']}',
+                        style: TextStyle(
+                          // fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    left: getProportionateScreenWidth(15),
+                    right: getProportionateScreenWidth(15),
+                    top: getProportionateScreenWidth(15),
+                  ),
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          splashColor: Colors.transparent,
+                          onTap: () {
+                            // tapFavourite();
+                          },
+                          // child: item.isFavorite
+                          //     ? Icon(Icons.favorite)
+                          //     : Icon(Icons.favorite_outline),
+                          child: Icon(Icons.favorite_outline),
+                        ),
+                        Text(
+                          '${docs['rating'].toString()}',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
